dbd77f7aa8e36d064a859ac0276c669148faaa9f
GummiGu <Augustus0508@outlook.com>
2024-11-10 17:00:47 +0800

    help加入

--- tools/help.py
+++ tools/help.py
@@ -1,14 +1,14 @@
 
 
 def help_info():
-    return (".r[num1]d[num2] 执行num1次数值1-num2的掷骰"
-            ".r[num1]d[num2][h|l] 奖励骰，取最高得分，惩罚骰，取最低得分"
-            ".ra [str] 对某属性进行判定"
-            ".login [str]根据口令进行登录，可私聊"
-            ".register 获取口令"
-            ".char gen 生成一个角色属性模板"
-            ".char add [str] 将刚生成的角色模板加入角色库"
-            ".char use [str] 使用角色"
-            ".char edit [str] [num] 设置角色属性为num"
-            ".char show 展示角色库角色"
+    return (".r[num1]d[num2] 执行num1次数值1-num2的掷骰\n"
+            ".r[num1]d[num2][h|l] 奖励骰，取最高得分，惩罚骰，取最低得分\n"
+            ".ra [str] 对某属性进行判定\n"
+            ".login [str]根据口令进行登录，可私聊\n"
+            ".register 获取口令\n"
+            ".char gen 生成一个角色属性模板\n"
+            ".char add [str] 将刚生成的角色模板加入角色库\n"
+            ".char use [str] 使用角色\n"
+            ".char edit [str] [num] 设置角色属性为num\n"
+            ".char show 展示角色库角色\n"
             )

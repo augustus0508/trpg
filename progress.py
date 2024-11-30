b3c1c3eb99fdd4695c019b31d045dcbdab38c99c
GummiGu <Augustus0508@outlook.com>
2024-11-30 21:31:49 +0800

    优化代码布局，增加错误处理

--- progress.py
+++ progress.py
@@ -34,7 +34,7 @@ def rd_process(user_name,user_id,context):
     # step 2 process
     # step 2.1 error return
     if check_message.state==False:
-        return "错误输入。 示例: .r[num]d[num][h|l]/.ra [STR|CON|SIZ|DEX|APP|INT|POW|KNO|LUC]"
+        return "Error: 示例: .r[num]d[num][h|l]/.ra [STR|CON|SIZ|DEX|APP|INT|POW|KNO|LUC]"
 
     # step 2.2 generate
     dice_consequence1 = dice.generate_random_integers(check_message.parameters[0], check_message.parameters[1])
@@ -45,8 +45,9 @@ def rd_process(user_name,user_id,context):
         dice_consequence2 = [min(dice_consequence1),]
 
     # step 2.3 reply
-    miao_reply=""
     reply_str = f'执行{check_message.parameters[0]}d{check_message.parameters[1]}    '
+    miao_reply=""
+
     if check_message.method == "h":
         reply_str += f"惩罚骰:{str(dice_consequence2)}    "
         miao_reply="\n"+joking.return_joke(dice_consequence2,check_message.parameters[1])
@@ -55,8 +56,10 @@ def rd_process(user_name,user_id,context):
         miao_reply = "\n" + joking.return_joke(dice_consequence2, check_message.parameters[1])
     elif check_message.parameters[0]==1:
         miao_reply = "\n" + joking.return_joke(dice_consequence1, check_message.parameters[1])
-    reply_str += f'{str(dice_consequence1)}'
+
+    reply_str += f'{dice_consequence1}'
     reply_str+=miao_reply
+
     return reply_str
 
 def ra_process(user_name,user_id,context):
@@ -66,12 +69,14 @@ def ra_process(user_name,user_id,context):
     # step 2 process
     # step 2.1 error return
     if check_message.state==False:
-        return "错误输入 示例: .ra [STR|CON|SIZ|DEX|APP|INT|POW|KNO|LUC|str]"
+        return "Error: 示例: .ra [STR|CON|SIZ|DEX|APP|INT|POW|KNO|LUC|str]"
     # step 2.2 generate
     temp_dice=dice.generate_random_integers(1,100)
+
     #step 2.2 reply
-    miao_reply=""
     reply_str = f'执行ra {check_message.method}    '
+    miao_reply=""
+
     if check_message.method in character.Character.attribute_names:
         user_id = user.User.login_id(user_id)#默认取到admin
         if user_id in attributes_cache and attributes_cache[user_id] is not None:
@@ -82,8 +87,10 @@ def ra_process(user_name,user_id,context):
             miao_reply = "\n" + joking.return_joke(temp_dice, 100)
     else:
         miao_reply = "\n" + joking.return_joke(temp_dice, 100)
+
     reply_str += f'{temp_dice}'
     reply_str+=miao_reply
+
     return reply_str
 
 def char_process(user_name, user_id, context):
@@ -93,54 +100,59 @@ def char_process(user_name, user_id, context):
     # step 2 process
     # step 2.1 error return
     if check_message.state==False:
-        return "错误输入 实例.char [add|use] [str]/.char edit [STR|CON|SIZ|DEX|APP|INT|POW|KNO|LUC] [num]/.char gen/.char show"
+        return "Error: 示例.char [add|use] [str]/.char edit [STR|CON|SIZ|DEX|APP|INT|POW|KNO|LUC] [num]/.char gen/.char show"
     if not user.User.is_logined(user_id):
-        return "请先登录或注册 实例: .login [str]/.register"
+        return "Error: 请先登录或注册 实例: .login [str]/.register"
+
     # step 2.2 replace user_id to login_user_id
     login_user_id = user.User.login_id(user_id)
+
     # step 2.3 reply
     if check_message.method == "add":
         if login_user_id in attributes_cache and attributes_cache[login_user_id] is not None:
             attributes_cache[login_user_id].name=check_message.parameters[0]
-            temp=attributes_cache[login_user_id].save_to_db()
-            if temp is not None:
-                return temp
-            else:
-                return "角色已加入"
+            err=attributes_cache[login_user_id].save_to_db()
+            if err is not None:
+                return err
+            return "角色已加入"
         else:
-            return "没有已生成数据"
+            return "Error: 没有已生成数据"
     elif check_message.method == "use":
-        temp=character.Character.load_from_db(check_message.parameters[0], login_user_id)
-        if temp is not None:
-            attributes_cache[login_user_id]=temp
+        err,data=character.Character.load_from_db(check_message.parameters[0], login_user_id)
+        if err is not None:
+            return err
+
+        if data is not None:
+            attributes_cache[login_user_id]=err
             return attributes_cache[login_user_id].print_character_info()
         else:
-            return "不存在指定角色"
+            return "Error: 不存在指定角色"
     elif check_message.method == "edit":
         if login_user_id in attributes_cache and attributes_cache[login_user_id] is not None:
-            temp=attributes_cache[login_user_id].update_attribute(check_message.parameters[0], check_message.parameters[1])
-            if temp is not None:
-                return temp
-            else:
-                return attributes_cache[login_user_id].print_character_info()
-        return
+            err=attributes_cache[login_user_id].update_attribute(check_message.parameters[0], check_message.parameters[1])
+            if err is not None:
+                return err
+            return attributes_cache[login_user_id].print_character_info()
     elif check_message.method == "gen":
         attributes_cache[login_user_id] = character.Character.generate_random_character("temp_" + user_name, login_user_id)
         return attributes_cache[login_user_id].print_character_info()
     elif check_message.method == "show":
-        temp=character.Character.query_by_owner(login_user_id)
-        if temp is not  None:
+        err,data=character.Character.query_by_owner(login_user_id)
+        if err is not None:
+            return err
+
+        if data is not  None:
             reply_str=f"\n{'_'*13}\n"
-            for i in temp:
+            for i in data:
                 reply_str+=f'{i:^13}\n'
             reply_str += f"{'_' * 13}\n"
             return reply_str
-        return "未创建角色"
+        return "Error: 未创建角色"
 
 def login_process(user_name,user_id,context):
     method=cT.check_login(context)
     if method.state==False:
-        return "密码错误"
+        return "Error: 密码错误"
     else:
         return user.User.login(method.method,user_id)
 
